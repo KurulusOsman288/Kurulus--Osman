14d5a3667bc02eda9238fcf660df713bd0b97b85
KurulusOsman288 <umartariq583@gmail.com>
2025-01-21 21:09:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript Example: Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
